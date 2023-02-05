2f5c7b466c59acb060e8ae194c41781183f63ed0
aaronthefknpanda <aaronscottkumar25@hotmail.com>
2023-02-04 20:05:47 -0500

    ConnectButton added to all puzzles

--- Global Game Jam 2023/Assets/Scripts/Puzzles/CypherPuzzle.cs
+++ Global Game Jam 2023/Assets/Scripts/Puzzles/CypherPuzzle.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class CypherPuzzle : MonoBehaviour
@@ -8,23 +9,29 @@ public class CypherPuzzle : MonoBehaviour
     public GameObject IncorrectMessage;
     public TextMeshProUGUI InputText;
 
+    public InputField m_InputText;
+    public string m_Text;
+    public string m_Answer;
+
     private void Awake()
     {
         IncorrectMessage.SetActive(false);
+        m_Text = m_InputText.text;
     }
 
 
     private void Update()
     {
-        if(InputText.text != "Kettle" && InputText.text != "")
+        if(m_InputText.text != "Kettle" || m_InputText.text != "")
         {
             IncorrectMessage.SetActive(true);
+
+            Debug.Log("That's the wrong word!");
         }
         else
         {
             IncorrectMessage.SetActive(false);
+            Debug.Log("You did it!");
         }
-
-
     }
 }
